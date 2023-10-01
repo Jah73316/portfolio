160cdd243790e2311ee0b0a9d4ff555675d89cc3
lulu108 <aweenkarim@gmail.com>
2023-09-30 21:35:43 -0500

    add bootstrap and new button

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   let title = 'welcome to my portfolio';
+  const handleOnClick = ()=>{
+    alert('Hello and Welcome')
+  }
   return (
     <div className="App">
       <p>{title.toUpperCase()}</p>
+      <button onClick={handleOnClick} type="button" class="btn btn-primary">Primary</button>
     </div>
   );
 }
