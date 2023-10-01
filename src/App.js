4eeb53a4348d997fedc809e0081607674bf5a92a
lulu108 <aweenkarim@gmail.com>
2023-09-30 21:11:12 -0500

    first commit by me

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let title = 'welcome to my portfolio';
+  return (
+    <div className="App">
+      <p>{title.toUpperCase()}</p>
+    </div>
+  );
+}
+
+export default App;
